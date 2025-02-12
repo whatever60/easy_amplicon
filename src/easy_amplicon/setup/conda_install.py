413666e2dc99ae9814a213d57cb3904653789ecc
whatever60 <quym@foxmail.com>
2025-02-11 20:35:20 -0500

    uncomment lines

--- src/easy_amplicon/setup/conda_install.py
+++ src/easy_amplicon/setup/conda_install.py
@@ -8,24 +8,30 @@ import argparse
 import subprocess
 import os
 
+
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--flavor", type=str, default="conda", choices=["conda", "mamba"])
+    parser.add_argument(
+        "--flavor", type=str, default="conda", choices=["conda", "mamba"]
+    )
     args = parser.parse_args()
     flavor = args.flavor
     requirement_file = f"{os.path.dirname(__file__)}/../data/conda_requirements.txt"
-    ret = subprocess.run(
-        [
-            flavor,
-            "install",
-            "--file",
-            requirement_file,
-            "-c",
-            "defaults",
-            "-c",
-            "bioconda",
-            "-c",
-            "conda-forge",
-        ]
-    )
-    assert ret.returncode == 0, f"Conda install failed with return code {ret.returncode}"
+    args = [
+        flavor,
+        "install",
+        "--file",
+        requirement_file,
+        "-c",
+        "defaults",
+        "-c",
+        "bioconda",
+        "-c",
+        "conda-forge",
+        "-y",
+    ]
+    print(" ".join(args))
+    ret = subprocess.run(args)
+    assert (
+        ret.returncode == 0
+    ), f"Conda install failed with return code {ret.returncode}"
